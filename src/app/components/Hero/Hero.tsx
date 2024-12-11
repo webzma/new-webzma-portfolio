c650881941e1b3f4368bb1b29e381c4890b38beb
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2024-12-11 01:16:02 -0400

    updated

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,23 +1,38 @@
+import Image from 'next/image';
+import { Button } from '../ui/Button';
+
 export function Hero() {
   return (
-    <section className="grid grid-cols-2 pt-60 ">
-      <div className="flex items-center justify-center">
-        <div className="size-[400px] border-2 border-black rounded-full shadow-2xl"></div>
+    <section className='grid grid-cols-2 pt-60 pb-[3000px] gap-x-16'>
+      <div className='flex items-center justify-center'>
+        <Image
+          src='/avatar.webp'
+          alt='avatar image'
+          width={500}
+          height={500}
+          className='relative size-[350px] border-4 border-black rounded-3xl shadow-2xl object-cover'
+        />
       </div>
 
-      <div className="flex flex-col gap-y-2">
-        <h1 className="text-6xl font-extrabold text-[#18181b]">
+      <div className='flex flex-col gap-y-4 justify-center pt-12'>
+        <h1 className='text-6xl font-extrabold text-[#18181b]'>
           Wilberk Ledezma
         </h1>
-        <h3 className="text-3xl text-gray-600">Desarrollador Frontend</h3>
-        <p className="text-pretty text-lg w-[490px]">
+        <h3 className='text-3xl text-gray-600'>Desarrollador Frontend</h3>
+        <p className='text-pretty text-lg w-[490px]'>
           Creando experiencias digitales únicas con código limpio y diseño
           intuitivo. Transformando ideas en realidad, una línea de código a la
           vez.
         </p>
+
+        <div className='flex gap-x-2 pt-6'>
+          <Button type='CV'>Descargar CV</Button>
+          <Button type='LINKEDIN'>Conectemos en LinkedIn</Button>
+          <Button type='GITHUB'>Visita mi GitHub</Button>
+        </div>
       </div>
 
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[600px] h-[700px] bg-[#c7c7c7] rounded-full blur-3xl opacity-20 "></div>
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-[#c7c7c7] rounded-full blur-3xl opacity-30 animate-pulse -z-10'></div>
     </section>
   );
 }
--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,11 +1,21 @@
+import clsx from 'clsx';
+
 interface ButtonProps {
   children: React.ReactNode;
+  type: string;
 }
 
-export default function Button({ children }: ButtonProps) {
+export const Button: React.FC<ButtonProps> = ({ children, type }) => {
   return (
-    <button className="bg-[#333] text-white px-4 py-2 rounded-lg">
+    <button
+      className={clsx(
+        'text-white px-6 py-2 rounded-lg border-none outline-none rounded-3xl font-bold',
+        type === 'CV' && 'bg-[#2563eb]',
+        type === 'LINKEDIN' && 'bg-white text-gray-900',
+        type === 'GITHUB' && 'bg-[#333] '
+      )}
+    >
       {children}
     </button>
   );
-}
+};
