dc8931ea81750a7c7a17a75662e5b750cf6fa3f0
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2025-01-27 19:00:16 +0100

    feat: remove unused FaEarthAmericas icon import from Hero component

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { FaEarthAmericas, FaGithub, FaLinkedin } from "react-icons/fa6";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export function Hero() {
   return (
